d843fc203657b510afbbbd29dd9d4d01ecf619ca
Abhinav <akumar261089@gmail.com>
2018-02-27 17:26:43 +0530

    Replacing Spinnaker ami with generic ubuntu and adding provisioning steps in provision script

--- files/aws/spinnaker/provision.sh
+++ files/aws/spinnaker/provision.sh
@@ -1,35 +1,42 @@
 #!/bin/sh
 
+##Halyard Installation
+curl -O https://raw.githubusercontent.com/spinnaker/halyard/master/install/debian/InstallHalyard.sh
+
+sudo bash InstallHalyard.sh
+
+## Environment selecion
+hal config deploy edit --type localdebian
+
+## Installing  Minio for storage
+wget https://dl.minio.io/server/minio/release/linux-amd64/minio
+
+chmod +x minio
+sudo mkdir -p /minio/data
+sudo ./minio server --address 0.0.0.0:9001  /minio/data/ &
+
+##Need to fetch credentials from .minio/config.json
+accessKey=`cat .minio/config.json  |  grep accessKey | cut -d\" -f4`
+secretKey=`cat .minio/config.json  |  grep secretKey | cut -d\" -f4`
+
+## configuring storage
+hal config storage edit --type s3
+
+echo $secretKey | hal config provider aws edit --access-key-id $accessKey   --secret-access-key 
+##version seletion
+hal config version edit --version 1.6.0
+##configure jenkins
+
+hal config ci jenkins enable
+
+echo $4 | hal config ci jenkins master add my-jenkins-master \
+    --address http://jenkins.$2:8000 \
+    --username $3 \
+    --password
+
+##
+hal deploy apply
+
+hal deploy connect
 
 
-cp /tmp/terraform/igor-local.yml /opt/spinnaker/config/
-sed -i.bak -e "s/JENKINS_URL/http:\/\/jenkins.$2\//" -e "s/JENKINS_USERNAME/$3/" -e "s/JENKINS_PASSWORD/$4/" /opt/spinnaker/config/igor-local.yml
-
-echo "deb http://jenkins.$2:8000/ binary/" > /etc/apt/sources.list.d/deb-internal.list
-
-
-#
-#	modify spinnaker-local.yml to set igor_enabled to True
-#
-
-sed -i.bak2 -e '/igor:/{ n;N;N;{d};}' /opt/spinnaker/config/spinnaker-local.yml
-sed -i.bak -e 's/igor:/igor:\n     enabled: true/' /opt/spinnaker/config/spinnaker-local.yml
-
-#sed -i.bak -e "s/igor_enabled\: false/igor_enabled\: true/" /opt/spinnaker/config/spinnaker-local.yml
-
-
-service igor start
-
-#
-#	The bakes, in this environment, need to use the local debian repository
-#
-sed -i.bak -e "s/# debianRepository: .*/debianRepository: http\:\/\/debianrepo.${2}:8000\/ binary\//" /opt/rosco/config/rosco.yml
-
-#
-#	Set the default region in settings.js and restart apache
-#
-sed -i.bak -e "s/let awsDefaultRegion = '.*'\;/let awsDefaultRegion = '$1'\;/" /var/www/settings.js
-sudo service apache2 restart
-
-
-sleep 5
